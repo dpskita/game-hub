3af8d459be83d43913ff14e5e9e234c0cca0a50d
Kita <99019577+dpskita@users.noreply.github.com>
2023-05-05 19:46:17 +0400

    Build game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+import apiClient from "../services/api-client";
 
-interface Game {
+export interface Game {
+    image: any;
     id: number;
     name: string;
+    background_image: string;
   }
   
   interface FetchGamesResponse {
